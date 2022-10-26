99bcbb24886eda34f5a4f794cccb5f236c8bbbc7
Hanno Braun <hanno@braun-odw.eu>
2022-10-26 14:26:20 +0200

    Replace C code with transpiled Rust code

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,8 @@
+mod predicates;
+
 use std::sync::Once;
 
 static ALREADY_INIT: Once = Once::new();
-
-mod predicates {
-    #[link(name = "predicates")]
-    extern "C" { 
-        pub fn exactinit();
-        pub fn orient2d(pa: &[f64;2], pb: &[f64;2], pc: &[f64;2]) -> f64;
-        pub fn orient3d(pa: &[f64;3], pb: &[f64;3], pc: &[f64;3], pd: &[f64;3]) -> f64;
-        pub fn incircle(pa: &[f64;2], pb: &[f64;2], pc: &[f64;2], pd: &[f64;2]) -> f64;
-        pub fn insphere(pa: &[f64;3], pb: &[f64;3], pc: &[f64;3], pd: &[f64;3], pe: &[f64;3]) -> f64;
-    }
-}
 
 pub fn exactinit() {
     ALREADY_INIT.call_once(|| {
@@ -23,24 +14,24 @@ pub fn exactinit() {
 
 pub fn orient2d(pa: &[f64;2], pb: &[f64;2], pc: &[f64;2]) -> f64 {
     unsafe {
-        predicates::orient2d(pa, pb, pc)
+        predicates::orient2d(pa.as_ptr(), pb.as_ptr(), pc.as_ptr())
     }
 }
 
 pub fn orient3d(pa: &[f64;3], pb: &[f64;3], pc: &[f64;3], pd: &[f64;3]) -> f64 {
     unsafe {
-        predicates::orient3d(pa, pb, pc, pd)
+        predicates::orient3d(pa.as_ptr(), pb.as_ptr(), pc.as_ptr(), pd.as_ptr())
     }
 }
 
 pub fn incircle(pa: &[f64;2], pb: &[f64;2], pc: &[f64;2], pd: &[f64;2]) -> f64 {
     unsafe {
-        predicates::incircle(pa, pb, pc, pd)
+        predicates::incircle(pa.as_ptr(), pb.as_ptr(), pc.as_ptr(), pd.as_ptr())
     }
 }
 
 pub fn insphere(pa: &[f64;3], pb: &[f64;3], pc: &[f64;3], pd: &[f64;3], pe: &[f64;3]) -> f64 {
     unsafe {
-        predicates::insphere(pa, pb, pc, pd, pe)
+        predicates::insphere(pa.as_ptr(), pb.as_ptr(), pc.as_ptr(), pd.as_ptr(), pe.as_ptr())
     }
 }
